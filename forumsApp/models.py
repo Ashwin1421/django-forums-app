e2e66f467f455f4a2d80f69c5349a0b38f339601
Ashwin Joshi <ashwin.joshi12@live.com>
2018-06-28 12:14:27 -0500

    updates

--- forumsApp/models.py
+++ forumsApp/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -16,13 +13,3 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
-
-
-class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
-
-    class Meta:
-        model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
