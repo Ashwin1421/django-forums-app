92298d4c78474c8ca9a7ca987af11242ec1ac8d6
Ashwin Joshi <ashwin.joshi12@live.com>
2018-07-09 00:51:24 -0500

    Added comment model

--- forumsApp/models.py
+++ forumsApp/models.py
@@ -14,3 +14,17 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+    
+    def __str__(self):
+        return self.text
