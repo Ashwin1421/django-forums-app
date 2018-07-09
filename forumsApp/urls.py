75048fe8cd470805dfc5bae19888387f5b70becd
Ashwin Joshi <ashwin.joshi12@live.com>
2018-07-09 00:53:01 -0500

    Added url end points for adding comments and detail view for article

--- forumsApp/urls.py
+++ forumsApp/urls.py
@@ -7,6 +7,8 @@ from . import views
 app_name = "forumsApp"
 urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
     path("", views.index, name="index"),
+    path("articles/", views.index, name="articles"),
+    path("article/<int:pk>/", views.article_detail_view, name="article-detail"),
     path("loginView/",views.loginView, name="loginView"),
     path("signup/", views.signup, name="signup"),
     path("logoutView/", views.logoutView, name="logout"),
@@ -14,4 +16,5 @@ urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
     path("accounts/profile/", views.index, name="profile"),
     path("profile/", views.profile, name="profileView"),
     path("changePassword/", views.change_password, name="updatePassword"),
+    path("^article/(?P<pk>\d+)/add_comment/$", views.add_comment, name="article_comment"),
 ]
